bc682e4ad1e0667b6b954dc9e77fc73abb121153
Natalja Savcuk <nataliya.s@centrum.cz>
2023-10-21 11:08:01 +0200

    Add project files.

---
+++ HomeWork-lekture3-Morse/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_lekture3_Morse
+{
+    public static class Class1
+    {
+        public static string Morse(char znak) 
+        {
+            string[] morseovyZnaky = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            string[] morseovyCisla = { "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+            string temp = "";
+
+
+            if ((znak >= '0') && (znak <= 'Z')) //Toto jsem ne vymyslela já, ani jsem nevěděla že tak to jde.
+                                                //Chtěla jsem napsat normalní array a abecedou a táhat s něj znaky v cyklu for.
+                                                //Manžel řekl že programátor musí byt líný a ukázal tuto cestu. :)
+            {
+                temp = morseovyZnaky[ znak - 'A'];
+            }
+            else
+            {
+                if ((znak >= '0') && (znak <= '9'))
+                {
+                    temp = morseovyCisla[znak - '0'];
+                }
+                else
+                {
+                    temp = znak.ToString();
+                }
+            }
+            return temp;
+        }
+    }
+}
---
+++ HomeWork-lekture3-Morse/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeWork_lekture3_Morse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj! Jmenuji se ROB v1.1 a umím základy Morseovy abecedy!\n" +
+                "Umím pouze základy, tak že prosím mluv na mě pomalu,\n" +
+                "pouze latinská písmena (bez diakritiky) a čísla.\n Jdeme na to a vyzkoušíme moje schopnosti!");
+            //Potřebuji nápovědu - jak vůbec najdu v ASCII kód pro znaky s diakritikou? A v jakém oni tam jsou pořadí?
+
+            Console.WriteLine("Zadej text:");
+            string text = Console.ReadLine().ToUpper(); 
+            char[] pismena = text.ToCharArray();
+
+            Console.WriteLine("V Morsovce to bude vzpadát přibližně tak: "); // Přibližně - protože CH lze napsat jako jeden znak ale i
+                                                                             // jako 2 znaky, a budou se lišít. A nevím jak mám
+                                                                             // (i jestli musím) převest do morseovky speciální znaky
+            Console.WriteLine();
+            for (int i = 0; i < pismena.Length; i++)
+            {
+                Console.Write(Class1.Morse(pismena[i])); //musíme znaky morseovky něják rozdělovat? Mezerou, čárkou?
+            }
+            Console.ReadLine();
+        }
+    }
+}
